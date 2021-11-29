4c9fd6d0e3bc2100f3ca4cffa402e37e88cc7e1c
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-11-29 13:30:58 +0200

    Added materials from lessons #8 and #9 (Write First Ruby File and Run)

---
+++ s01____introduction_and_installation/e08__write_first_ruby_file_and_run.rb
@@ -0,0 +1,8 @@
+# Write First Ruby File and Run
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6477430#overview) is the link to the video.
+
+# Notes:
+# 1. ruby files must have .rb extension
+# 2. run ruby file by `$ ruby <filename>.rb`
+
+puts "Hello, World!"

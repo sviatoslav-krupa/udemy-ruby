32bd6fe98aa9462eb9a1a5d314dfc54427d9e67e
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-11-30 12:47:46 +0200

    Added materials from lesson #23 (Constants)

---
+++ s02____getting_started/e023__constants.rb
@@ -0,0 +1,14 @@
+# Constants
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6477578#overview) is the link to the video.
+
+# Notes:
+# * Name starts with capital letter, best practice to make all letter capital
+# * Can be changed but warning will appear
+
+NAME = "Sviatoslav"
+PI = 3.14159
+
+NAME = "John"
+# s02____getting_started/e023__constants.rb:11: warning: already initialized constant NAME
+# s02____getting_started/e023__constants.rb:8: warning: previous definition of NAME was here
+# => anyway, NAME = "John"

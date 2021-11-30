de5fe4f86188eb4b654f4943afad948f40ad619a
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-11-30 11:14:33 +0200

    Added materials from lesson #20 (comments)

---
+++ s02____getting_started/e020__comments.rb
@@ -0,0 +1,11 @@
+# Comments
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6477572#overview) is the link to the video.
+# Comments are ignored by interpreter
+
+# Single line comment (everything after '#' is ignored), preferred even for multiline comments
+puts "Hello, World!" # This text is ignored
+
+=begin
+  Multiline
+  comment
+=end

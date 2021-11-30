7ec466d9a887f7ab7ec64fcfa19a73cabbf0a925
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-11-30 10:52:59 +0200

    Added materials from lesson #19 (basic arithmetic)

---
+++ s02____getting_started/e019__basic_arithmetic.rb
@@ -0,0 +1,34 @@
+# Basic arithmetic
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6477570#overview) is the link to the video.
+
+# Addition
+p 1 + 4 # => 5
+p "School" + "bus" # => "Schoolbus" (string concatenation)
+
+# Subtraction
+p 10 - 6 # => 4
+
+# Multiplication
+p 3 * 4 # => 12
+
+# Division
+p 10 / 5 # => 2
+p 12 / 5 # => 2 (integer / integer => integer)
+p 12.0 / 5 # => 2.4 (float / integer => float)
+p 12 / 5.0 # => 2.4 (integer / float => float)
+p 12.0 / 5.0 # => 2.4 (float / float => float)
+# p .5 / 2 # => no .<digit> floating literal anymore; put 0 before dot p .5 / 2
+p 0.5 / 2 # => 0.25
+
+# Exponents (to the power of <number>)
+p 2 ** 4 # => 16
+p 3 ** 2 # => 9
+
+# Modulo operator (return remainder of division)
+p 5 % 2 # => 1
+p 14 % 4 # => 2
+p 6 % 2 # => 0
+
+# Notes
+p 1 + 4 # (1 + 4) called expression
+# Multiplication operator '*' called asterisk

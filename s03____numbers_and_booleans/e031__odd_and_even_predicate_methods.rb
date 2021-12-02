2cef1179036e903c965be89940ec8c9ff69ba523
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-02 11:02:23 +0200

    Added materials from lesson #31 (The .odd? and .even? Predicate Methods)

---
+++ s03____numbers_and_booleans/e031__odd_and_even_predicate_methods.rb
@@ -0,0 +1,14 @@
+# The .odd? and .even? Predicate Methods
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6477652#overview) is the link to the video.
+
+# .odd? (непарне?)
+p 1.odd? # => true
+p 2.odd? # => false
+
+# .even? (парне?)
+p 1.even? # => false
+p 2.even? # => true
+
+# .odd? and .even? called only on Integer
+p 4.2.odd? # => undefined method `odd?' for 4.2:Float (NoMethodError)
+p "4.2".odd? # =>  undefined method `odd?' for "4.2":String (NoMethodError)

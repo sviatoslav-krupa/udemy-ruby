52ba94c78fcb11a43ae67b7cc1f454d2eb099017
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-02 11:12:37 +0200

    Added materials from lesson #32 (Comparison with the Equality (==) Operator)

---
+++ s03____numbers_and_booleans/e032__equality_operator.rb
@@ -0,0 +1,23 @@
+# Comparison with the Equality (==) Operator
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6477654#overview) is the link to the video.
+
+# Checks if 2 values are identical or equal
+p 10 == 10 # => true
+p 10 == 20 # => false
+p 10 == "10" # => false
+p 10 == 10.0 # => true (even here are Integer and Float)
+p 10 == 10.1 # => false
+
+a = 10
+b = 5
+c = 10
+
+p a == c # => true
+p a == b # => false
+p b == c # => false
+
+# Placement are irrelevant
+p a == c # => true
+p c == a # => true
+p a == b # => false
+p b == a # => false

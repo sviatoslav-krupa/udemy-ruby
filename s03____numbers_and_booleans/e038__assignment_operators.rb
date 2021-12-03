1f6a028660b0de7a7a9cdff84d8df19461ed5b3f
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-03 14:59:09 +0200

    Added materials from lesson #38 (Assignment Operators)

---
+++ s03____numbers_and_booleans/e038__assignment_operators.rb
@@ -0,0 +1,29 @@
+# Assignment Operators
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6477668#overview) is the link to the video.
+
+# Simple assignment
+a = 10
+p a # => 10
+
+a = a + 5 # => a = 10 + 5  => a = 15 (right side evaluates first)
+p a # => 15
+
+# Shorthands:
+
+a = a + 5
+a += 5 # shorthand
+
+a = a - 3
+a -= 3 # shorthand
+
+a = a * 2
+a *= 2 # shorthand
+
+a = a / 3
+a /= 3 # shorthand
+
+a = a ** 3
+a **= 3 # shorthand
+
+a = a % 6
+a %= 6 # shorthand

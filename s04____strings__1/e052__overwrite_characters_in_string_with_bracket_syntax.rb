791d49bd4219c14c7f29630ee9051e55ae513380
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-07 11:59:50 +0200

    Add more edge cases

---
+++ s04____strings__1/e052__overwrite_characters_in_string_with_bracket_syntax.rb
@@ -0,0 +1,36 @@
+# Overwrite Characters in String with Bracket Syntax
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6481506#overview) is the link to the video.
+
+thing = "rocket ship"
+p thing # => "rocket ship"
+
+# Overwriting single character
+thing[0] = "p"
+p thing # => "pocket ship"
+
+thing[1] = "a"
+p thing # => "packet ship"
+
+thing[9] = "o"
+p thing # => "packet shop"
+
+thing[-100] = "test" # => index -100 out of string (IndexError)
+
+# Overwriting multiple characters
+fact = "I love blueberry pie"
+p fact # => "I love blueberry pie"
+
+fact[7, 4] = "rasp"
+fact[7..10] = "rasp"
+fact[7...11] = "rasp"
+# fact.slice(7...11) = "rasp" (syntax error, unexpected '=', expecting end-of-input (SyntaxError) fact.slice(7, 4) = "rasp")
+p fact # => "I love raspberry pie"
+
+fact[2..5] = "absolutely adore"
+p fact # => "I absolutely adore raspberry pie"
+
+# Edge cases
+fact[7, -4] = "rasp" # => negative length -4 (IndexError)
+fact[7..100] = "rasp" # => "I love rasp"
+fact[7..-50] = "rasp" # => "I love raspblueberry pie"
+fact[-70..3] = "rasp" # => -70..3 out of range (RangeError)

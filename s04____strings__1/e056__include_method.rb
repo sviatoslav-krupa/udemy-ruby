8026b0f98f7652b905206e527a336b8c3ae6c581
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-08 10:37:27 +0200

    Added materials from lesson #56 (The .include? Method on a String)

---
+++ s04____strings__1/e056__include_method.rb
@@ -0,0 +1,25 @@
+# The .include? Method on a String
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6481524#overview) is the link to the video.
+
+# .include? => true | false - check if a substring exists in a string
+
+name = "Snow White 123"
+
+p name.include?("S") # => true
+p name.include?("h") # => true
+p name.include?(" ") # => true
+p name.include?("  ") # => false
+p name.include?("s") # => false (case sensitivity)
+p name.downcase.include?("s") # => true
+p name.upcase.include?("OW") # => true
+
+p name.include?("snow") # => false
+p name.include?("Snow") # => true
+p name.include?("rain") # => false
+p name.include?("it") # => true
+p name.include?("sit") # => false
+
+# Edge cases
+p name.include?(123) # => no implicit conversion of Integer into String (TypeError)
+p name.include?('123') # => true
+p name.include?('nW') # => false (check ordering of characters)

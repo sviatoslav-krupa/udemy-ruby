cebd26de4b47bf5fe44237ba922ceb32270d398a
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-13 11:18:16 +0200

    Added materials from lesson #79 (The until Keyword)

---
+++ s06____methods_and_conditionals__2/e079__until_keyword.rb
@@ -0,0 +1,11 @@
+# The until Keyword
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6481740#overview) is the link to the video.
+
+# until = while not (the same as unless and if)
+
+i = 1
+until i > 9
+  # Evaluates body of a loop til condition is true
+  puts i
+  i += 1
+end # => nil (prints 1 2 3 4 5 6 7 8 9)

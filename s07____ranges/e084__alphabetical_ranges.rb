4004fe5823bfa25edc408aba196c7386c1d6c00c
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-14 10:23:37 +0200

    Added materials from lesson #84 (Alphabetical Ranges)

---
+++ s07____ranges/e084__alphabetical_ranges.rb
@@ -0,0 +1,14 @@
+# Alphabetical Ranges
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6481778#overview) is the link to the video.
+
+lowercase_alphabet = "a".."z"
+p lowercase_alphabet.first(5) # => ["a", "b", "c", "d", "e"]
+p lowercase_alphabet.last(8) # => ["s", "t", "u", "v", "w", "x", "y", "z"]
+
+uppercase_alphabet = "A".."Z"
+p uppercase_alphabet.first(5) # => ["A", "B", "C", "D", "E"]
+p uppercase_alphabet.last(8) # => ["S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+all_cases_alphabet = "A".."z"
+p all_cases_alphabet.first(40)
+# => ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h"]

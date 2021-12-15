ce6b3ea14d883447b00639cea875e250307176b4
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-15 10:19:21 +0200

    Added materials from lesson #90 (Shorthand %w Syntax to Create an Array of Strings)

---
+++ s08____arrays_1__creation_addition_modification/e090__shorthand_%w_syntax_to_create_array_of_strings.rb
@@ -0,0 +1,6 @@
+# Shorthand %w Syntax to Create an Array of Strings
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482082#overview) is the link to the video.
+
+# the space is a beginning of a new string, new array element
+names = %w[Jack Jill John James Sviatoslav Krupa]
+p names # => ["Jack", "Jill", "John", "James", "Sviatoslav", "Krupa"]

56ca7a5ba25747de98eb923684caf928b1435586
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-15 10:03:14 +0200

    Added materials from lesson #88 (Introduction to Arrays)

---
+++ s08____arrays_1__creation_addition_modification/e088__intro_to_arrays.rb
@@ -0,0 +1,16 @@
+# Introduction to Arrays
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482078#overview) is the link to the video.
+
+# Documentation (https://ruby-doc.org/core-2.3.3/Array.html)
+
+# What is an array:
+# * Ordered collection of objects (as a list).
+# * Can contain objects with different types.
+# * Itself is an object.
+# * It acts as a container for other objects.
+# * An array is created with a pair of square brackets `[]`.
+
+p [] # => [] (empty array)
+p [].class # => Array
+
+numbers = []

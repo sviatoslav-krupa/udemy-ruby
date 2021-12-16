0ff82a739b970253fdd71bf603f3fad2129a8cc5
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-16 11:01:48 +0200

    Added materials from lesson #100 (The .empty? and .nil? Methods on an Array)

---
+++ s08____arrays_1__creation_addition_modification/e100__empty?_and_nil?_methods.rb
@@ -0,0 +1,26 @@
+# The .empty? and .nil? Methods on an Array
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482140#overview) is the link to the video.
+
+# .empty? - true if length == 0 (no elements)
+p [1, 2, 3].empty? # => false
+p [].empty? # => true
+p [false, false].empty? # => false
+p [nil, nil].empty? # => false
+
+# nil? - true if object is a NilClass
+p [1, 2, 3].nil? # => false
+p [].nil? # => false
+p [false, false].nil? # => false
+p [nil, nil].nil? # => false
+p nil.nil? # => true
+
+letters = ("a".."j").to_a
+p letters # => ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+character = letters[5]
+p character # => "f"
+p character.nil? # => false
+
+character = letters[25]
+p character # => nil
+p character.nil? # => true

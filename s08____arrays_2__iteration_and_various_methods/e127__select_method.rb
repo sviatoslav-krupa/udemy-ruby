45c0954b3b3a7f8e735014290308ed0a10b1afe8
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-21 12:34:41 +0200

    Added materials from lesson #127 (The .select Method on an Array)

---
+++ s08____arrays_2__iteration_and_various_methods/e127__select_method.rb
@@ -0,0 +1,12 @@
+# The .select Method on an Array
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482360#overview) is the link to the video.
+
+# .select - extracts elements that fir certain condition
+
+grades = [80, 95, 13, 76, 28, 39]
+matches = grades.select { |grade| grade >= 75 } # => [80, 95, 76]
+p matches
+
+words = %w[level selfless racecar dinosaur]
+palindromes = words.select { |word| word == word.reverse }
+p palindromes # => ["level", "racecar"]

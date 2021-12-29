8a761a4d1376e309dae0064b689f788a9b0eb433
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2021-12-29 10:26:15 +0200

    Added materials from lesson #167 (The .key? and .value? Methods on a Hash)

---
+++ s13____hashes_2/e167__key?_and_value?_methods.rb
@@ -0,0 +1,29 @@
+# The .key? and .value? Methods on a Hash
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482500#overview) is the link to the video.
+
+cars = {
+  toyota: "camry",
+  chevrolet: "aveo",
+  ford: "F-150",
+  kia: "soul"
+}
+
+# .key? - returns true if the given key is present in hash.
+cars.key?(:toyota) #=> true
+cars.key?("toyota") #=> false (String is not the same as Symbol)
+cars.key?(:bmw) #=> false
+
+# Deprecated
+cars.has_key?(:toyota) #=> true
+cars.has_key?("toyota") #=> false (String is not the same as Symbol)
+cars.has_key?(:bmw) #=> false
+
+# .value? - returns true if the given value is present for some key in hash.
+cars.value?("aveo") #=> true
+cars.value?(:aveo) #=> false (Symbol is not the same as String)
+cars.key?("civic") #=> false
+
+# Depreacted
+cars.has_value?("aveo") #=> true
+cars.has_value?(:aveo) #=> false (Symbol is not the same as String)
+cars.has_key?("civic") #=> false

23ef09be2c148161c87332fdfc32498823bd9032
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-11 11:01:07 +0200

    Added materials from lesson #219 (Getter Methods)

---
+++ s18____classes__1/e219__getter_methods.rb
@@ -0,0 +1,31 @@
+# Getter Methods
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482618#search) is the link to the video.
+
+# getters - for get instance variables from an object
+# instance methods have access to all instance variables and can call other instance methods
+
+class Gadget
+  def initialize
+    @username = "User #{rand(1..100)}"
+    @password = "topsecret"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
+
+  def username
+    @username
+  end
+
+  def production_number
+    @production_number
+  end
+
+  def to_s
+    "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} class and it has the id #{self.object_id}"
+  end
+end
+
+phone = Gadget.new
+
+phone.username #=> "User 21"
+phone.production_number #=> "c-628"
+phone.password #=> undefined method `password' for #<Gadget:0x00007f81d008e8d8 @username="User 54", @password="topsecret", @production_number="z-261"> (NoMethodError)

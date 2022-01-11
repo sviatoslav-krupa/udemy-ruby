3b4df30b64a1e91a46f9d1ba47ed7490b245e559
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-11 09:53:22 +0200

    Added materials from codding exercise #6 (Create Classes with Instance Variables)

---
+++ s18____classes__1/coding_exercise_6__create_class_with_instance_variables.rb
@@ -0,0 +1,10 @@
+# Coding Exercise 6: Create Classes with Instance Variables
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/quiz/5421720#search) is the link to the exercise.
+
+class Musical
+  def initialize
+    @name = "Test name"
+    @cast = "Test cast"
+    @duration = "Test duration"
+  end
+end

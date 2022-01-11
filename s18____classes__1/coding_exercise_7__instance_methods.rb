700d049cd3a5938fc9a3e7da4e3b3acf450bfdfe
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-11 10:05:23 +0200

    Added materials from codding exercise #7 (Instance Methods)

---
+++ s18____classes__1/coding_exercise_7__instance_methods.rb
@@ -0,0 +1,16 @@
+# Coding Exercise 7: Instance Methods
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/quiz/5421732#search) is the link to the exercise.
+
+class Cake
+  def bake
+    "Baking the cake"
+  end
+
+  def slice
+    "Slicing the cake"
+  end
+
+  def sell
+    "Sold the cake"
+  end
+end

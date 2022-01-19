6dfc49eb1958d4e6690a33917599c4af02f627b8
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-19 12:11:37 +0200

    Added materials from lesson #240 (Monkey Patching II)

---
+++ s20____classes__2/e240__monkey_patching_2.rb
@@ -0,0 +1,43 @@
+# Monkey Patching II
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482660#search) is the link to the video.
+
+class Hash
+  def identify_duplicate_values
+    values = []
+    duplicates = []
+
+    self.each_value do |value|
+      values.include?(value) ? duplicates << value : values << value
+    end
+
+    duplicates.uniq
+  end
+end
+
+{
+  a: 1,
+  b: 2,
+  c: 1,
+  d: 3,
+  e: 1
+}.identify_duplicate_values #=> [1]
+
+
+class Fixnum
+  def custom_times
+    i = 1
+    while i <= self
+      yield(i)
+      i +=1
+    end
+  end
+end
+
+5.custom_times { |time| p time }
+=begin
+  1
+  2
+  3
+  4
+  5
+=end

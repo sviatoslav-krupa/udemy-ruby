44af29bca00ecbb5afb1e49e25405a7d2ec5e4bc
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-19 12:00:51 +0200

    Added materials from lesson #239 (Monkey Patching I)

---
+++ s20____classes__2/e239__monkey_patching_1.rb
@@ -0,0 +1,16 @@
+# Monkey Patching I
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482658#search) is the link to the video.
+
+# Monkey Patching - adding features to predefined, existing class
+
+[1, 2, 3, true, "Hello", :ok].custom_sum #=> undefined method `custom_sum' for [1, 2, 3, true, "Hello", :ok]:Array (NoMethodError)
+
+class Array
+  def custom_sum
+    total = 0
+    self.each { |item| total += item if item.is_a?(Numeric) }
+    total
+  end
+end
+
+[1, 2, 3, true, "Hello", :ok].custom_sum #=> 6

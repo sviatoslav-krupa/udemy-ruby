a8a850c1bc8d7a42eb3f7ee7f161e808f1a80164
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-24 10:14:00 +0200

    Added materials from lesson #246 (The .is_a? and .instance_of? Predicate Methods)

---
+++ s21____classes__3/e246__is_a?_and_instance_of?_predicate_methods.rb
@@ -0,0 +1,36 @@
+# The .is_a? and .instance_of? Predicate Methods
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482674#search) is the link to the video.
+
+class Employee
+  attr_accessor :age
+  attr_reader :name
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    "Hi, my name is #{name} and I am #{age} years old!"
+  end
+end
+
+class Manager < Employee; end
+class Worker < Employee; end
+
+bob = Manager.new("Bob", 45)
+dan = Worker.new("Dan", 34)
+
+# .is_a? - true if object inherits from the class that passed in an argument
+bob.is_a?(Manager) #=> true
+bob.is_a?(Employee) #=> true
+bob.is_a?(Object) #=> true
+dan.is_a?(Manager) #=> false
+bob.is_a?(String) #=> false
+
+# .instance_of? - true if object directly inherits from the class that passed in an argument (more strict)
+bob.instance_of?(Manager) #=> true
+bob.instance_of?(Employee) #=> false
+dan.instance_of?(Worker) #=> true
+dan.instance_of?(Employee) #=> true
+dan.instance_of?(Manager) #=> false

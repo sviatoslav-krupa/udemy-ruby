b59eeb9c5637a6ac7fa16cb3f83bd47c3d051430
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-24 09:57:05 +0200

    Added materials from lesson #244 (Create Subclasses)

---
+++ s21____classes__3/e244__create_subclasses.rb
@@ -0,0 +1,31 @@
+# Create Subclasses
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482670#search) is the link to the video.
+
+class Employee
+  attr_accessor :age
+  attr_reader :name
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    "Hi, my name is #{name} and I am #{age} years old!"
+  end
+end
+
+class Manager < Employee; end
+class Worker < Employee; end
+
+sviat = Employee.new("Sviat", 24)
+bob = Manager.new("Bob", 45)
+dan = Worker.new("Dan", 34)
+
+sviat.class #=> Employee
+bob.class #=> Manager
+dan.class #=> Worker
+
+sviat.introduce #=> "Hi, my name is Sviat and I am 24 years old!"
+bob.introduce #=> "Hi, my name is Bob and I am 45 years old!"
+dan.introduce #=> "Hi, my name is Dan and I am 34 years old!"

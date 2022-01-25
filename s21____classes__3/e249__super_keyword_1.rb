ece1d764c6d589f0f8e9defa3e52a8bfd98196ff
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-25 09:26:51 +0200

    Added materials from lesson #249 (The super Keyword I)

---
+++ s21____classes__3/e249__super_keyword_1.rb
@@ -0,0 +1,40 @@
+# The super Keyword I
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482682#search) is the link to the video.
+
+# .super - execute the same superclass method
+
+class Employee
+  attr_accessor :age
+  attr_reader :name
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    "Hi, my name is #{name} and I am #{age} years old!"
+  end
+end
+
+class Manager < Employee
+  attr_reader :rank
+
+  def initialize(name, age, rank)
+    super(name, age)
+    @rank = rank
+  end
+
+  def introduce
+    super + " And I'm also a manager with a rank of #{rank}!"
+  end
+end
+
+sviat = Employee.new("Sviat", 24)
+bob = Manager.new("Bob", 45, 3)
+
+sviat.rank #=> undefined method `rank' for #<Employee:0x00007fd0db0c98e8 @name="Sviat", @age=24> (NoMethodError)
+bob.rank #=> 3
+
+sviat.introduce #=> "Hi, my name is Sviat and I am 24 years old!"
+bob.introduce #=> "Hi, my name is Bob and I am 45 years old! And I'm also a manager with a rank of 3!"

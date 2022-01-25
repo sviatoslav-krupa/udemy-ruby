2b4517ce9ef98abc5442fa5db6f0548d95bc1234
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-25 10:01:21 +0200

    Added materials from lesson #251 (Class Variables across Subclasses)

---
+++ s21____classes__3/e251__class_variables_across_subclasses.rb
@@ -0,0 +1,53 @@
+# Class Variables across Subclasses
+# Here (https://www.udemy.com/course/learn-to-code-with-ruby-lang/learn/lecture/6482686#search) is the link to the video.
+
+class Product
+  @@product_counter = 0
+
+  def initialize
+    @@product_counter += 1
+  end
+
+  def self.product_counter
+    @@product_counter
+  end
+end
+
+class Widget < Product
+  @@widget_counter = 0
+
+  def initialize
+    super
+    @@widget_counter += 1
+  end
+
+  def self.widget_counter
+    @@widget_counter
+  end
+end
+
+class Thingamajig < Product
+  @@thingamajig_counter = 0
+
+  def initialize
+    super
+    @@thingamajig_counter += 1
+  end
+
+  def self.thingamajig_counter
+    @@thingamajig_counter
+  end
+end
+
+Widget.new
+Thingamajig.new
+
+Product.product_counter #=> 2
+Widget.widget_counter #=> 1
+Thingamajig.thingamajig_counter #=> 1
+
+# Edge cases
+Widget.product_counter #=> 2 (Widget has access to :product_counter - class variable of super class)
+Thingamajig.product_counter #=> 2
+Product.widget_counter #=> undefined method `widget_counter' for Product:Class (NoMethodError)
+Product.thingamajig_counter #=> undefined method `thingamajig_counter' for Product:Class (NoMethodError)

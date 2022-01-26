da5eda09595a5c0dbab5a8eed8b4c6d87e7047a9
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-01-26 10:33:51 +0200

    Added materials from lessons #255 and #259 (Use rdoc to Generate Your Own Documentation)

---
+++ s22____reading_and_writing_documentation/e258_e259__use_rdoc_to_generate_your_own_documentation/album.rb
@@ -0,0 +1,28 @@
+# An Album class that stores an array of songs
+class Album
+  include Enumerable
+
+  # An Array object of songs. Each song is a String
+  attr_reader :songs
+
+  # Creates a new Album instance with an empty songs array
+  def initialize
+    @songs = []
+  end
+
+  # Add a song to the album object's songs array
+  def add_song(song)
+    songs << song
+  end
+
+  # Yield each song in the album to a block
+  def each
+    songs.each { |song| yield(song) }
+  end
+end
+
+thriller = Album.new
+thriller.add_song("Thriller")
+thriller.add_song("Billie Jean")
+
+thriller.songs #=> ["Thriller", "Billie Jean"]

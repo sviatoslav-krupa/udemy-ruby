d57b472f8147fdbe41907a28d592c80f74a38919
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-07-26 10:25:28 +0300

    Fixed typo

--- s08____arrays_1__creation_addition_modification/e096__access_multiple_array_elements_with_values_at_method.rb
+++ s08____arrays_1__creation_addition_modification/e096__access_multiple_array_elements_with_values_at_method.rb
@@ -13,7 +13,7 @@ p channels.values_at(1, -1, -2) # =>  ["FOX", "UPN", "ESPN"]
 # Can pass duplicate arguments
 p channels.values_at(3, 3, 4) # =>  ["ESPN", "ESPN", "UPN"]
 
-# Arguments have tp be passed by comma (not in array)
+# Arguments have to be passed by comma (not in array)
 p channels.values_at([1, 2]) # => no implicit conversion of Array into Integer (TypeError)
 
 # Edge cases

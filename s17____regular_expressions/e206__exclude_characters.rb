594f3900278189fabfd7db2de8a95a61b6f76add
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2022-07-27 12:39:12 +0300

    Fixed typo

--- s17____regular_expressions/e206__exclude_characters.rb
+++ s17____regular_expressions/e206__exclude_characters.rb
@@ -3,5 +3,5 @@
 
 sales = "I bought 9 apples, 25 bananas and 4 oranges at the store."
 
-# ignore [^...] elements, except this elements
+# ignore [^...] elements
 sales.scan(/[^aeiouAEIOU,\s\d\.]/) #=> ["b", "g", "h", "t", "p", "p", "l", "s", "b", "n", "n", "s", "n", "d", "r", "n", "g", "s", "t", "t", "h", "s", "t", "r"]

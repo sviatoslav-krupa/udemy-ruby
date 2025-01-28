7ca524ec1a8d8df8556657df4df8f37cd2621c30
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2025-01-28 12:46:57 +0200

    One more update

--- s06___methods/05__default_or_optional_parameters.rb
+++ s06___methods/05__default_or_optional_parameters.rb
@@ -1,5 +1,9 @@
 # Default or Optional Parameters
 
+def make_phone_call(number, international_code = 1, area_code = 646) # => syntax error found (SyntaxError): unexpected parameter order
+  puts "Calling #{international_code}-#{area_code}-#{number}"
+end
+
 make_phone_call(1234567, 5, 999) # => Calling 5-999-1234567
 make_phone_call(1234567,4) # => Calling 4-646-1234567
 make_phone_call(1234567) # => Calling 1-646-1234567
@@ -11,6 +15,6 @@ make_phone_call(1234567) # => Calling 1-646-1234567
 make_phone_call(1234567, nil, 5) # => Calling -5-1234567
 
 # Default parameters must be pushed to the end.
-def make_phone_call(number = 123456, international_code, area_code = 646) # => syntax error found (SyntaxError): unexpected parameter order
+def another_make_phone_call(number = 123456, international_code, area_code = 646) # => syntax error found (SyntaxError): unexpected parameter order
   puts "Calling #{international_code}-#{area_code}-#{number}"
 end

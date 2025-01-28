39aba8928b3f1fa4f3f989db3449fc83d2cdc77a
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2025-01-28 12:46:07 +0200

    One more update

--- s06___methods/05__default_or_optional_parameters.rb
+++ s06___methods/05__default_or_optional_parameters.rb
@@ -1,11 +1,5 @@
 # Default or Optional Parameters
 
-# Default parameters must be pushed to the end.
-
-def make_phone_call(number, international_code = 1, area_code = 646)
-  puts "Calling #{international_code}-#{area_code}-#{number}"
-end
-
 make_phone_call(1234567, 5, 999) # => Calling 5-999-1234567
 make_phone_call(1234567,4) # => Calling 4-646-1234567
 make_phone_call(1234567) # => Calling 1-646-1234567
@@ -15,3 +9,8 @@ make_phone_call(1234567) # => Calling 1-646-1234567
 # Arguments are passed in the same order as parameters.
 # We can't skip first optional parameter and fill only second (we can skip parameters only at the end).
 make_phone_call(1234567, nil, 5) # => Calling -5-1234567
+
+# Default parameters must be pushed to the end.
+def make_phone_call(number = 123456, international_code, area_code = 646) # => syntax error found (SyntaxError): unexpected parameter order
+  puts "Calling #{international_code}-#{area_code}-#{number}"
+end

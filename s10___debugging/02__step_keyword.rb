4d3e397c24d4daa1ffb7ac297c8ca17369b64029
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2025-02-04 12:00:54 +0200

    Update section 10

---
+++ s10___debugging/02__step_keyword.rb
@@ -0,0 +1,15 @@
+# step | s - execute the current line and move on to the next line.
+
+require "debug"
+
+debugger
+
+puts "Hello".downcase
+puts "Goodbye".upcase
+
+debugger
+
+3.times do |count|
+  puts "On loop number #{count}"
+  puts "Blah blah blah"
+end

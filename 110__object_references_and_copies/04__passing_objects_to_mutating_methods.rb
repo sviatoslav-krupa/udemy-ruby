3923bfedafbc387a8fa23adc97645c9582517f82
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2025-03-12 15:04:12 +0200

    Some refactoring

--- 110__object_references_and_copies/04__passing_objects_to_mutating_methods.rb
+++ 110__object_references_and_copies/04__passing_objects_to_mutating_methods.rb
@@ -4,7 +4,7 @@ def append_5(elements) # `elements` is an alias (one more pointer) to `values` i
 end
 
 append_5(values)
-values # => [1, 2, 3, 4, 5]
+values #=> [1, 2, 3, 4, 5]
 
 
 name = "Boris"
@@ -13,19 +13,17 @@ def uppercase(text) # `text` is an alias (one more pointer) to `name` in this ca
 end
 
 uppercase(name)
-name # => "BORIS"
+name #=> "BORIS"
 
 new_name = "Sviat".freeze
-uppercase(new_name) # => can't modify frozen String: "Sviat" (FrozenError)
-new_name # => "Sviat"
+uppercase(new_name) #=> can't modify frozen String: "Sviat" (FrozenError)
+new_name            #=> "Sviat"
 
-
-# Edge cases
-
+# Edge cases:
 new_values = [1, 2, 3, 4]
 def new_example(elements) # `elements` is an alias (one more pointer) to `values` in this case
   elements = [] # assigns new object to `elements` variable (pointer), `new_values` variable (pointer) not changed
 end
 
 new_example(new_values)
-new_values # => [1, 2, 3, 4]
+new_values #=> [1, 2, 3, 4]

ceecfa578fa5c2ac7a7a5908d49e22379172ec0e
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-01-07 18:07:15 +0200

    Fix argument error in praise_person method
    Updated praise_person method to require two arguments.

--- 05__methods/02__parameters_and_arguments.rb
+++ 05__methods/02__parameters_and_arguments.rb
@@ -9,7 +9,7 @@ praise_person("Sviat", 24) #=> Sviat (24 y.o.) is amazing!
 # "David" and 35 is an argument
 praise_person "David", 35 #=> David (35 y.o.) is amazing!
 
-praise_person #=> wrong number of arguments (given 0, expected 1) (ArgumentError)
+praise_person #=> wrong number of arguments (given 0, expected 2) (ArgumentError)
 
 # Manipulation Arguments
 def praise_person(name, age)

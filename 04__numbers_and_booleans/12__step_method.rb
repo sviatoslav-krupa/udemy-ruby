527d60bacb30333eda11449b43fa5ff5833e10b5
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-01-08 11:40:18 +0200

    Update step method examples in 12__step_method.rb

--- 04__numbers_and_booleans/12__step_method.rb
+++ 04__numbers_and_booleans/12__step_method.rb
@@ -11,7 +11,7 @@
 #   9
 
 # Iterates from 1 to -10 with step -2 (subtracts 2 on each step, like .downto method), returns first value
-1.step(-10, -2) { |count| puts count } #=> 1 (prints 1, -1, -3, -5, -7, -9)
+1.step(-10, -2) { |count| puts count } #=> 1
 # OUTPUT:
 #   1
 #   -1
@@ -21,7 +21,7 @@
 #   -9
 
 # Iterates from 1 to 10 with step 2.5 (works with float as a step), returns first value
-1.step(10, 2.5) { |count| puts count } #=> 1 (prints 1.0, 3.5, 6.0, 8.5)
+1.step(10, 2.5) { |count| puts count } #=> 1
 # OUTPUT:
 #   1.0
 #   3.5
@@ -29,7 +29,7 @@
 #   8.5
 
 # Iterates from 1.2 to 10.5 with step 2 (works with float), returns first value
-1.2.step(10.5, 2) { |count| puts count } #=> 1.2 (prints 1.2, 3.2, 5.2, 7.2, 9.2)
+1.2.step(10.5, 2) { |count| puts count } #=> 1.2
 # OUTPUT:
 #   1.2
 #   3.2

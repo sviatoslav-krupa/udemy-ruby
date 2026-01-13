b653e00bdebeef2cd1ae6c016f74b7c4d15db6c2
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-01-13 12:07:46 +0200

    Update 11__extract_multiple_characters_with_range_object.rb

--- 07__strings/11__extract_multiple_characters_with_range_object.rb
+++ 07__strings/11__extract_multiple_characters_with_range_object.rb
@@ -7,7 +7,7 @@ story = "Once upon a time in a land far, far away"
 
 story.length #=> 40
 
-# Gets substring from 27 index to 40 index inclusively (39 index will be included; 27..39 is a range)
+# Gets substring from 27 index to 39 index inclusively (39 index will be included; 27..39 is a range)
 story[27..39]       #=> "far, far away"
 story.slice(27..39) #=> "far, far away"
 

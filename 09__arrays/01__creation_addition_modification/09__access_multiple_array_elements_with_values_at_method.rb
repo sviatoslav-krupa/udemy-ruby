465b0d466aa672de0f2f60b3af6826f3873de4a5
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-01-15 13:19:45 +0200

    Update examples for values_at method in Ruby

--- 09__arrays/01__creation_addition_modification/09__access_multiple_array_elements_with_values_at_method.rb
+++ 09__arrays/01__creation_addition_modification/09__access_multiple_array_elements_with_values_at_method.rb
@@ -5,10 +5,10 @@
 
 channels = %w[CBS FOX NBC ESPN UPN]
 
-channels.values_at(1)         #=> ["FOX"]
-channels.values_at(1, 2)      #=> ["FOX", "NBC"]
-channels.values_at(1, 2, 4)   #=> ["FOX", "NBC", "UPN"]
-channels.values_at(3, 3, 4) #=>  ["ESPN", "ESPN", "UPN"]
+channels.values_at(1)       #=> ["FOX"]
+channels.values_at(1, 2)    #=> ["FOX", "NBC"]
+channels.values_at(1, 2, 4) #=> ["FOX", "NBC", "UPN"]
+channels.values_at(3, 3, 4) #=> ["ESPN", "ESPN", "UPN"]
 
 # Edge cases:
 channels.values_at([1, 2])          #=> no implicit conversion of Array into Integer (TypeError)

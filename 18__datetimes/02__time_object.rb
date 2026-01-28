c8728dfcc3aa290baf0204b6bc0610ed84f0a4a9
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-01-28 19:44:45 +0200

    Update 02__time_object.rb

--- 18__datetimes/02__time_object.rb
+++ 18__datetimes/02__time_object.rb
@@ -16,13 +16,13 @@ another_today = Time.now
 another_today       #=> 2022-01-03 09:51:48.061619 +0200
 another_today.class #=> Time
 
-Time.now(2015)                                                   #=> wrong number of arguments (given 1, expected 0) (ArgumentError)
-Time.new                                                         #=> 2022-01-03 09:53:13.058111 +0200
-Time.new(2015)                                              #=> 2015-01-01 00:00:00 +0200 (the start of the 2015 year)
-Time.new(2015, 5)                                    #=> 2015-05-01 00:00:00 +0300 (the start of the 2015 year, May (5) month)
-Time.new(2015, 5, 18)                           #=> 2015-05-18 00:00:00 +0300 (the start of the 2015 year, May (5) month, 18 day)
-Time.new(2015, 5, 18, 23)                  #=> 2015-05-18 23:00:00 +0300
-Time.new(2015, 5, 18, 23, 45)         #=> 2015-05-18 23:45:00 +0300
+Time.now(2015)                    #=> wrong number of arguments (given 1, expected 0) (ArgumentError)
+Time.new                          #=> 2022-01-03 09:53:13.058111 +0200
+Time.new(2015)                    #=> 2015-01-01 00:00:00 +0200 (the start of the 2015 year)
+Time.new(2015, 5)                 #=> 2015-05-01 00:00:00 +0300 (the start of the 2015 year, May (5) month)
+Time.new(2015, 5, 18)             #=> 2015-05-18 00:00:00 +0300 (the start of the 2015 year, May (5) month, 18 day)
+Time.new(2015, 5, 18, 23)         #=> 2015-05-18 23:00:00 +0300
+Time.new(2015, 5, 18, 23, 45)     #=> 2015-05-18 23:45:00 +0300
 Time.new(2015, 5, 18, 23, 45, 05) #=> 2015-05-18 23:45:05 +0300
 
 today.class      #=> Time
@@ -47,6 +47,6 @@ today.sec        #=> 1
 today.dst? #=> false
 
 # Edge cases:
-Time.new(2100)            #=> 2100-01-01 00:00:00 +0200 (for future)
-Time.new(-1000)           #=> -1000-01-01 00:00:00 +0300 (even with negative year)
+Time.new(2100)     #=> 2100-01-01 00:00:00 +0200 (for future)
+Time.new(-1000)    #=> -1000-01-01 00:00:00 +0300 (even with negative year)
 Time.new(2022, 30) #=> argument out of range (ArgumentError)

794c92818e6bbc11fe53d46362522841f9675d4a
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-02-02 12:53:26 +0200

    Fix formatting of area method calls

--- 17__modules_and_mixins/03__import_modules_into_current_file.rb
+++ 17__modules_and_mixins/03__import_modules_into_current_file.rb
@@ -3,6 +3,6 @@
 
 require_relative '02__modules_with_identical_methods' # file with modules
 
-Square.area(5)                #=> 25
+Square.area(5)        #=> 25
 Rectangle.area(3, 6)  #=> 18
-Circle.area(9)              #=> 254.46878999999998
+Circle.area(9)        #=> 254.46878999999998

02ddd1c1e41d2c50bc73ac640ff12d2e8362bef4
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-02-02 13:58:03 +0200

    Fix formatting of ancestors method output

--- 17__modules_and_mixins/09__ancestors_method_in_depth.rb
+++ 17__modules_and_mixins/09__ancestors_method_in_depth.rb
@@ -12,15 +12,15 @@ class Bookstore
     "You bought a copy of #{item}!"
   end
 end
-Bookstore.ancestors                           #=> [Bookstore, Purchasable, Object, JSON::Ext::Generator::GeneratorMethods::Object, PP::ObjectMixin, Kernel, BasicObject]
+Bookstore.ancestors                      #=> [Bookstore, Purchasable, Object, JSON::Ext::Generator::GeneratorMethods::Object, PP::ObjectMixin, Kernel, BasicObject]
 Bookstore.new.purchase("Atlas Shrugged") #=> "You bought a copy of Atlas Shrugged!"
 
 class Supermarket
   include Purchasable
 end
-Supermarket.ancestors                       #=> [Supermarket, Purchasable, Object, JSON::Ext::Generator::GeneratorMethods::Object, PP::ObjectMixin, Kernel, BasicObject]
+Supermarket.ancestors                  #=> [Supermarket, Purchasable, Object, JSON::Ext::Generator::GeneratorMethods::Object, PP::ObjectMixin, Kernel, BasicObject]
 Supermarket.new.purchase("Ice cream")  #=> "Ice cream has been purchased!"
 
 class CornerMart < Supermarket; end
-CornerMart.ancestors                      #=> [CornerMart, Supermarket, Purchasable, Object, JSON::Ext::Generator::GeneratorMethods::Object, PP::ObjectMixin, Kernel, BasicObject]
+CornerMart.ancestors                 #=> [CornerMart, Supermarket, Purchasable, Object, JSON::Ext::Generator::GeneratorMethods::Object, PP::ObjectMixin, Kernel, BasicObject]
 CornerMart.new.purchase("Slim Jim")  #=> "Slim Jim has been purchased!"

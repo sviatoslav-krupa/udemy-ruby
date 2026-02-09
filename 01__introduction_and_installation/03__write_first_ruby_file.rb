7fdc7f0f33fd1bc47006512cf9153c2bcbc3ce5b
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2026-02-09 17:43:30 +0200

    Update 03__write_first_ruby_file.rb

--- 01__introduction_and_installation/03__write_first_ruby_file.rb
+++ 01__introduction_and_installation/03__write_first_ruby_file.rb
@@ -1,6 +1,7 @@
 # Notes:
 #   * Ruby files must have .rb extension
 #   * Run ruby file by `$ ruby <filename>.rb`
+#   * `exit` in ruby file - exit a Ruby program file :)
 
 puts "Hello, World!" #=> nil
 # OUTPUT:
